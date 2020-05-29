0b61534ab5f3ce4fafcfe28d70e7462ee145714a
jangbigom91 <jangbigom91@gmail.com>
2020-05-29 09:04:57 +0900

    2020/05/29 Ch15 교재예제 등록

--- Ch15/src/p766/ComparatorExample.java
+++ Ch15/src/p766/ComparatorExample.java
@@ -1,5 +1,22 @@
 package p766;
 
-public class ComparatorExample {
+import java.util.Iterator;
+import java.util.TreeSet;
 
+import p765.DescendingComparator;
+
+public class ComparatorExample {
+	public static void main(String[] args) {
+		
+		TreeSet<Fruit> treeSet = new TreeSet<Fruit>(new DescendingComparator());
+		treeSet.add(new Fruit("포도", 3000));
+		treeSet.add(new Fruit("수박", 10000));
+		treeSet.add(new Fruit("딸기", 6000));
+		
+		Iterator<Fruit> iterator = treeSet.iterator();
+		while(iterator.hasNext()) {
+			Fruit fruit = iterator.next();
+			System.out.println(fruit.name + ":" + fruit.price);
+		}
+	}
 }
